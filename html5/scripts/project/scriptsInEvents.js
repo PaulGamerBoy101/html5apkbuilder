dc679fa873df305843ac1a796ea5e525cc8daa53
PaulGamerBoy101 <166418808+PaulGamerBoy101@users.noreply.github.com>
2024-12-14 21:08:33 -0700

    Add files via upload

---
+++ html5/scripts/project/scriptsInEvents.js
@@ -0,0 +1,64 @@
+
+
+
+const scriptsInEvents = {
+
+	async Startscreenevents_Event10(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async GameEvents_Event11(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async AboutScreenEvents_Event6(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async RedhouseEvents_Event6(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async Game2Events_Event11(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async YellowHouseEvents_Event6(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async PinkHouseEvents_Event6(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async OrangeHouseEvents_Event6(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async PurpleHouseEvents_Event6(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async MintHouseEvents_Event6(runtime, localVars)
+	{
+		document.body.style.cursor = "none";
+	},
+
+	async Game3Events_Event5(runtime, localVars)
+	{
+		document.body.style.cursor = "auto";
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
